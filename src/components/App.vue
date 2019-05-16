e107ec3f708dc95b44821e4b1be0386ed59f7a89
Jang Haemin <io@jhaemin.com>
2019-05-16 22:54:05 +0900

    Create the starting point of the app

---
+++ src/components/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="summer-togo-list-app">
+    <TodoList/>
+  </div>
+</template>
+
+<script>
+import TodoList from 'Components/TodoList'
+
+export default {
+  name: 'App',
+  components: { TodoList }
+}
+</script>
+
+<style lang="scss" scoped>
+#summer-todo-list-app {
+
+}
+</style>
