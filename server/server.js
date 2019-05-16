6bfa6c21df1234e48f4a14eaedb4cd5d00114966
Jang Haemin <io@jhaemin.com>
2019-05-16 22:52:48 +0900

    Set server for testing

---
+++ server/server.js
@@ -0,0 +1,6 @@
+var express = require('express');
+var app = express();
+app.use(express.static('./public_html'))
+var server = app.listen(3000, function(){
+    console.log("Express server has started on port 3000")
+})
