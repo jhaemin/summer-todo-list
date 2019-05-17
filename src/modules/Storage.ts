1446e6d4692fe0f66f55e7633280e9c70a3546c6
Jang Haemin <io@jhaemin.com>
2019-05-17 19:00:50 +0900

    Create sample data

--- src/modules/Storage.ts
+++ src/modules/Storage.ts
@@ -1,8 +1,8 @@
 // define todo item data structure
 interface TodoItem {
   title: string,
-  createdAt: string, // milliseconds
-  duedate: string|null, // milliseconds
+  createdAt: number, // milliseconds
+  duedate: number|null, // milliseconds
   priority: 1|2|3,
   isDone: boolean
 }
@@ -31,5 +31,68 @@ export default class Storage {
   static storeList(list: TodoItem) {
     // console.log(JSON.stringify(list, null, 2))
     window.localStorage.setItem('summer-todo-list', JSON.stringify(list))
+  }
+
+  static generateSamples() {
+    let sample: Array<TodoItem> = [
+      {
+        title: 'Email team for  updates',
+        createdAt: 1558082653426,
+        duedate: 1564412400000,
+        priority: 3,
+        isDone: false
+      },
+      {
+        title: 'Book conference room',
+        createdAt: 1558082653426,
+        duedate: null,
+        priority: 1,
+        isDone: false
+      },
+      {
+        title: 'Send out meeting request',
+        createdAt: 1558082653426,
+        duedate: null,
+        priority: 2,
+        isDone: false
+      },
+      {
+        title: 'Make a presentation for next meeting',
+        createdAt: 1558082653426,
+        duedate: 1563548400000,
+        priority: 2,
+        isDone: false
+      },
+      {
+        title: 'Create meeting agenda',
+        createdAt: 1558082653426,
+        duedate: null,
+        priority: 1,
+        isDone: false
+      },
+      {
+        title: 'Update project plan',
+        createdAt: 1558082653426,
+        duedate: null,
+        priority: 2,
+        isDone: false
+      },
+      {
+        title: 'Verify project budget',
+        createdAt: 1558082653426,
+        duedate: 1563548400000,
+        priority: 3,
+        isDone: true
+      },
+      {
+        title: 'Contact service center',
+        createdAt: 1558082653426,
+        duedate: null,
+        priority: 1,
+        isDone: true
+      }
+    ]
+
+    window.localStorage.setItem('summer-todo-list', JSON.stringify(sample))
   }
 }
