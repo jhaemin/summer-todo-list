3962b19c786ee26cb0f822d2aba131a140b08b49
Jang Haemin <io@jhaemin.com>
2019-05-17 21:08:17 +0900

    Apply transition to the list items

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -7,20 +7,23 @@
         v-model="newTaskTitle"
       >
     </div>
-    <ol id="todo-list" v-if="todoList.length > 0">
+    <!-- <ol id="todo-list" v-if="todoList.length > 0"> -->
+    <transition-group name="list-complete" tag="ol" id="todo-list" v-if="todoList.length > 0">
       <TodoItem
+        class="list-complete-item"
         v-for="(item, i) in todoList"
-        :key="item.title + item.createdAt"
+        :key="item.createdAt"
         :info="item"
         :index="i"
         @delete="deleteTask(i)"
         @done="doneTask(i, true)"
         @undone="doneTask(i, false)"
-        @updateTitle="updateTttle"
+        @updateTitle="updateTitle"
         @setDueDate="setDueDate"
         @setPriority="setPriority"
       />
-    </ol>
+    </transition-group>
+    <!-- </ol> -->
     <div class="empty-todo-list" v-else>
       <p class="title">No Task Yet<br><button class="example" @click="loadSample">Load Samples</button></p>
       <p class="manifesto">
@@ -47,6 +50,8 @@ export default {
     }
   },
   methods: {
+    // add task and update the task list data
+    // to the localStorage
     addTask() {
       // do nothing when the string is empty
       if (this.newTaskTitle.trim().length === 0) {
@@ -63,21 +68,26 @@ export default {
 
       // clear input field and defocus
       this.newTaskTitle = ''
+      this.$el.querySelector('.input-title').blur()
 
       // insert new item to the first
       this.todoList.unshift(newTask)
 
       // stores modified data
       Storage.storeList(this.todoList)
+      this.animateOnFirstLoad()
     },
+    // set the task done status
     doneTask(index, isDone) {
       this.todoList[index].isDone = isDone
       Storage.storeList(this.todoList)
     },
-    updateTttle(index, newTitle) {
+    // update the title
+    updateTitle(index, newTitle) {
       this.todoList[index].title = newTitle
       Storage.storeList(this.todoList)
     },
+    // set duedate
     setDueDate(index, year, month, day) {
       if (year === false) {
         this.todoList[index].duedate = null
@@ -87,22 +97,43 @@ export default {
 
       Storage.storeList(this.todoList)
     },
+    // set priority
     setPriority(index, priority) {
       this.todoList[index].priority = priority
       Storage.storeList(this.todoList)
     },
+    // delete the task
     deleteTask(index) {
       this.todoList.splice(index, 1)
       Storage.storeList(this.todoList)
     },
+    // load the sample date
     loadSample() {
       Storage.generateSamples()
       this.todoList = Storage.loadList()
+      this.animateOnFirstLoad()
+    },
+    // spring animation when the list is loaded
+    animateOnFirstLoad() {
+      console.log('animation called')
+      setTimeout(() => {
+        let items = this.$el.querySelectorAll('.todo-item-spring-wrapper:not(.spring)')
+        let i = 0
+        let interval = window.setInterval(() => {
+          if (!items[i] || i >= items.length) {
+            window.clearInterval(interval)
+            return
+          }
+          items[i].classList.add('spring')
+          i++
+        }, 50)
+      }, 10)
     }
   },
-  created() {
+  mounted() {
     // load saved todo list
     this.todoList = Storage.loadList()
+    this.animateOnFirstLoad()
   }
 }
 </script>
@@ -110,9 +141,31 @@ export default {
 <style lang="scss" scoped>
 @import 'Styles/global-variables';
 
+.list-complete-item {
+  transition: transform 800ms cubic-bezier(.43,.18,.17,1.09), opacity 200ms ease;
+}
+.list-complete-enter, .list-complete-leave-to
+/* .list-complete-leave-active below version 2.1.8 */ {
+  opacity: 0 !important;
+  transform: scale(0.9) !important;
+}
+.list-complete-leave-active {
+  position: absolute !important;
+  // width: 100% !important;
+}
+
+.cool-enter-active, .cool-leave-active {
+  transition: opacity 300ms ease !important;
+  opacity: 1 !important;
+}
+.cool-enter, .cool-leave-to {
+  opacity: 0 !important;
+}
+
 .todo-container {
   width: calc(100% - 3rem);
   max-width: 30rem;
+  min-height: 120vh;
   margin: auto;
   padding-bottom: 5rem;
 
@@ -140,6 +193,7 @@ export default {
 
   #todo-list {
     margin-top: 2rem;
+    position: relative;
   }
 
   .empty-todo-list {
